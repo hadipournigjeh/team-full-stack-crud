cdcb7c2f548abe4cd3321037e9516d926042739f
ZahraaTayyar <zahraatayyar@gmail.com>
2023-06-01 20:48:32 +0100

    routing done, delete student  button done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./student.css";
 import Student from "./Student";
+import CreateStudent from "./CreateStudent";
+import UpdateStudent from "./UpdateStudent";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <Student />
+      <Routes>
+        <Route path="/" element={<Student />}></Route>
+        <Route path="/create" element={<CreateStudent />}></Route>
+        <Route path="/update/:id" element={<UpdateStudent />}></Route>
+      </Routes>
     </div>
   );
 };
--- client/src/Student.jsx
+++ client/src/Student.jsx
@@ -1,25 +1,66 @@
 import "./student.css";
-import React from "react";
-import exampleresponse from "./exampleresponse.json";
+import React, { useState } from "react"; //add useEffect when connecting server.
+import studentData from "./exampleresponse.json";
+import { Link } from "react-router-dom";
+// import axios from "axios";  //Axios is already installed, no need to install again.
 
 function Student() {
-  const students = exampleresponse;
+  const [student, setStudent] = useState(studentData);
+
+  // TO BE ADDED WHEN SERVER IS SET UP:
+  // useEffect(() => {
+  //   fetch("http://localhost:8081/")
+  //   .then((res) => res.json())
+  //   .then((data) => setStudent(data.student))
+  //   .catch((error) => console.log(error));
+  // }, []);
+
+  //this code is only until server is set up:
+  function handleDelete(id) {
+    const updatedStudents = student.filter((stu) => stu.id !== id);
+    setStudent(updatedStudents);
+  }
+
+  // TO BE ADDED WHEN SERVER IS SET UP.
+  // const  handleDelete = async (id) => {
+  //   try {
+  //     await axios.delete(`http://localhost:8081/student/${id}`)
+  //     window.location.reload()
+  //   } catch(err) {
+  //     console.log(err)
+  //   }
+  // }
 
   return (
     <div className="main-div">
       <div className="card">
+        <Link to="/create" className="btn add-btn">
+          Add +
+        </Link>
         <table className="table">
           <thead>
             <tr>
               <th className="th">Name</th>
               <th>Email</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {students.map((student, index) => (
+            {student.map((student, index) => (
               <tr key={student.id}>
                 <td>{student.name}</td>
                 <td>{student.email}</td>
+                <td className="button-data-table">
+                  <Link to="/update/:id" className="btn update-btn">
+                    Update
+                  </Link>
+                  <button
+                    className="btn delete-btn"
+                    onClick={(e) => handleDelete(student.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
