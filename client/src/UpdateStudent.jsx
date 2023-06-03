b0ddd67bdc4175eed821dcb1722f2f7d5da674f7
lexiisying <nyxxing0824@gmail.com>
2023-06-04 00:17:44 +0100

    create&update forms created, need more styling.

--- client/src/UpdateStudent.jsx
+++ client/src/UpdateStudent.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import { useNavigate, useParams } from 'react-router-dom';
 
 function UpdateStudent() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  // const {id} = useParams();
+  // const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // // axios.put(""+id, {name,email})
+    // .then(res => {
+    //   console.log(res)
+    //   navigate("/")
+    // }).catch(err => console.log(err));
+  }
+
   return (
-    <div>UpdateStudent</div>
+    <div className='main-div'>
+      <div className='card' >
+        <form onSubmit={handleSubmit}>
+          <h2>
+            Update Student
+          </h2>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" placeholder='Enter Name' className='form-control' 
+            onChange={e => setName(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="text" placeholder='Enter Email' className='form-control'
+            onChange={e => setEmail(e.target.value)} />
+          </div>
+          <button className='btn'>Update</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
