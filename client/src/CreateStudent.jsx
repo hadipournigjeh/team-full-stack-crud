c683012452e4ab99431ab2ddb9d689269cb82676
Bahare <108987748+Bahare09@users.noreply.github.com>
2023-06-04 11:35:59 +0100

    Revert "create&update forms created, need more styling."

--- client/src/CreateStudent.jsx
+++ client/src/CreateStudent.jsx
@@ -1,41 +1,8 @@
-import React, { useState } from 'react';
-// import { Navigate, useNavigate } from 'react-router-dom';
+import React from 'react';
 
 function CreateStudent() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  // const navigate = useNavigate();
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    // // axios.post("", {name,email})
-    // .then(res => {
-    //   console.log(res)
-    //   navigate("/")
-    // }).catch(err => console.log(err));
-  }
-
   return (
-    <div className='main-div'>
-      <div className='card' >
-        <form onSubmit={handleSubmit}>
-          <h2>
-            Add Student
-          </h2>
-          <div>
-            <label htmlFor="">Name</label>
-            <input type="text" placeholder='Enter Name' className='form-control' 
-            onChange={e => setName(e.target.value)}/>
-          </div>
-          <div>
-            <label htmlFor="">Email</label>
-            <input type="text" placeholder='Enter Email' className='form-control'
-            onChange={e => setEmail(e.target.value)} />
-          </div>
-          <button className='btn'>Submit</button>
-        </form>
-      </div>
-    </div>
+    <div>CreateStudent</div>
   )
 }
 
