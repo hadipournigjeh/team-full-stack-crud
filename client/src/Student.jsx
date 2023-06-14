e8f9c19541fdfb8d474c1bdba62dcc1a517c50e0
ZahraaTayyar <zahraatayyar@gmail.com>
2023-06-15 00:44:44 +0100

    setstudent returned to how it was

--- client/src/Student.jsx
+++ client/src/Student.jsx
@@ -11,7 +11,7 @@ function Student() {
   useEffect(() => {
     fetch("http://localhost:5000/")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => setStudent(data))
       .catch((error) => console.log(error));
   }, []);
 
